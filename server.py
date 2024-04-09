ce0cfd82c29941aa4e2e321abfe7f9be32cd9002
Veronica H <vth@Veronicas-MacBook-Pro.local>
2024-04-09 13:44:52 -0400

    completed CBL challenge

--- server.py
+++ server.py
@@ -51,6 +51,9 @@ def increase_score():
         if team["id"] == team_id:
             team["score"] += 1
 
+    # need to sort the scoreboard(list) in non-increasing order, second element of dict/tuple
+    scoreboard.sort(key=lambda x: x["score"], reverse=True)
+
     return jsonify(scoreboard=scoreboard)
 
 
--- static/scoreboard.js
+++ static/scoreboard.js
@@ -13,6 +13,7 @@ function addTeamView(id, name, score){
   var increase_button = $("<button class = increase-button>+</button>");
   $(increase_button).click(function(){
     increase_score(id);
+
   });
   name_template.text(name);
   score_template.text(score);
@@ -32,7 +33,10 @@ function increase_score(id){
     contentType: "application/json; charset=utf-8",
     data : JSON.stringify(team_id),
     success: function(result){
-        
+      scoreboard = result.scoreboard;
+      //update interface
+      display_scoreboard(scoreboard);
+
     },
     error: function(request, status, error){
         console.log("Error");
